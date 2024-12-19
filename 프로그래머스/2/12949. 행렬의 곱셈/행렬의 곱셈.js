14f6ae4c22ebf117255578f5184deb889603126f
Jinri <127276737+zzznly@users.noreply.github.com>
2024-12-19 23:16:44 +0900

    [level 2] Title: 행렬의 곱셈, Time: 32.34 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -4,7 +4,8 @@ function solution(arr1, arr2) {
     const r2 = arr2.length;
     const c2 = arr2[0].length;
     
-    const res = [];
+    const res = []; // 결과를 저장할 2차원 배열
+    
     for (let i = 0; i < r1; i++) {
         res.push(new Array(c2).fill(0));
     }
@@ -12,7 +13,7 @@ function solution(arr1, arr2) {
     for (let i = 0; i < r1; i++) {
         for (let j = 0; j < c2; j++) {
             for (let k = 0; k < c1; k++) {
-                res[i][j] += arr1[i][k] * arr2[k][j];
+                res[i][j] += arr1[i][k] * arr2[k][j]
             }
         }
     }
