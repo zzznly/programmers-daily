ad2630ab1f59fff26984dd5307b1b581f9977ab1
zzznly <zzznly@gmail.com>
2025-01-28 00:26:50 +0900

    mod: 다른사람의 풀이 추가

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,15 @@
+// Best
 function solution(strings, n) {
-    return strings.sort((a, b) => {
-        if (a[n] > b[n]) return 1; // 오름차순
-        else if (a[n] < b[n]) return -1; // 내림차순
-        else return a > b ? 1 : -1
-    })
+  return strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1; // 오름차순
+    else if (a[n] < b[n]) return -1; // 내림차순
+    else return a > b ? 1 : -1; // 사전순 비교 (n번째 문자열 같을 경우)
+  });
+}
+
+// #1
+function solution(strings, n) {
+  return strings.sort((s1, s2) =>
+    s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n])
+  );
 }
